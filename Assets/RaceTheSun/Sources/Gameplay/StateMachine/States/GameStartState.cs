cd38059c3b15c1aa3334942831814df1378a08e6
Virvon <dan130205@gmail.com>
2024-07-05 20:10:14 +0300

    Install cinemachine

---
+++ Assets/RaceTheSun/Sources/Gameplay/StateMachine/States/GameStartState.cs
@@ -0,0 +1,27 @@
+﻿using Assets.RaceTheSun.Sources.Infrastructure.Factories.GameplayFactory;
+using Assets.RaceTheSun.Sources.Infrastructure.GameStateMachine;
+using Cysharp.Threading.Tasks;
+using System;
+
+namespace Assets.RaceTheSun.Sources.Gameplay.StateMachine.States
+{
+    public class GameStartState : IState
+    {
+        private readonly GameplayFactory _gameplayFactory;
+
+        public GameStartState(GameplayFactory gameplayFactory)
+        {
+            _gameplayFactory = gameplayFactory;
+        }
+
+        public async UniTask Enter()
+        {
+            
+        }
+
+        public UniTask Exit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
