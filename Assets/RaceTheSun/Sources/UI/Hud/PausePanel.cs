36f9ccf2afd5c029ed315566fcd7e55a9ed960be
Virvon <dan130205@gmail.com>
2024-07-23 12:43:42 +0300

    Animating pause panel

---
+++ Assets/RaceTheSun/Sources/UI/Hud/PausePanel.cs
@@ -0,0 +1,26 @@
+﻿using Assets.RaceTheSun.Sources.Services.TimeScale;
+using Zenject;
+
+namespace Assets.RaceTheSun.Sources.Animations
+{
+    public class PausePanel : HudAnimationElement
+    {
+        private ITimeScale _timeScale;
+
+        [Inject]
+        private void Construct(ITimeScale timeSclale) =>
+            _timeScale = timeSclale;
+
+        public override void Open()
+        {
+            _timeScale.Scale(TimeScaleType.Pause);
+            base.Open();
+        }
+
+        public override void Hide()
+        {
+            _timeScale.Scale(TimeScaleType.Normal);
+            base.Hide();
+        }
+    }
+}
