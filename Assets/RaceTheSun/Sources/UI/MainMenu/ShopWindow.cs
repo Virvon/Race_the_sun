0e2583e7714242861c1d9324a15f6f7012567c40
Virvon <dan130205@gmail.com>
2024-08-14 18:21:30 +0300

    Refactoring shop

--- Assets/RaceTheSun/Sources/UI/MainMenu/ShopWindow.cs
+++ Assets/RaceTheSun/Sources/UI/MainMenu/ShopWindow.cs
@@ -1,9 +1,31 @@
-﻿using System;
+﻿using Agava.YandexGames;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
 
 namespace Assets.RaceTheSun.Sources.UI.MainMenu
 {
     public class ShopWindow : OpenableWindow
     {
+        [SerializeField] private Button _button;
+        [SerializeField] private int _reward;
+
+        private IPersistentProgressService _persistentProgressService;
+
+        [Inject]
+        private void Construct(IPersistentProgressService persistentProgressService)
+        {
+            _persistentProgressService = persistentProgressService;
+
+            _button.onClick.AddListener(OnButtonClick);
+        }
+
+        private void OnDestroy()
+        {
+            _button.onClick.RemoveListener(OnButtonClick);
+        }
+
         public override void Hide()
         {
             gameObject.SetActive(false);
@@ -12,6 +34,18 @@ namespace Assets.RaceTheSun.Sources.UI.MainMenu
         public override void Open()
         {
             gameObject.SetActive(true);
+        }
+
+        private void OnButtonClick()
+        {
+#if !UNITY_EDITOR && UNITY_WEBGL
+            InterstitialAd.Show(onCloseCallback: (_) =>
+            {
+                _persistentProgressService.Progress.Wallet.Take(_reward);
+            });
+#else
+            _persistentProgressService.Progress.Wallet.Take(_reward);
+#endif
         }
     }
 }
